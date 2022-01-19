73e6e72089a4d734f858c85f6581ffdb69ca2c2d
finessiano <80067457+finessiano@users.noreply.github.com>
2022-01-20 00:57:50 +0100

    Add files via upload

---
+++ dapp.js
@@ -0,0 +1,419 @@
+
+
+// contract address on Kovan:
+const rewardProgramAddress = '0xcb137a0ABA7E90A77782c0c1F1aC139Bc39A8EB7'
+
+// add contract ABI:
+const rewardProgramABI =
+[
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "approved",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "ApprovalForAll",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_data",
+				"type": "bytes"
+			}
+		],
+		"name": "safeTransferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "approved",
+				"type": "bool"
+			}
+		],
+		"name": "setApprovalForAll",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "getApproved",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			}
+		],
+		"name": "isApprovedForAll",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ownerOf",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes4",
+				"name": "interfaceId",
+				"type": "bytes4"
+			}
+		],
+		"name": "supportsInterface",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenURI",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+// Using the 'load' event listener for Javascript to
+// check if window.ethereum is available
+
+window.addEventListener('load', function() {
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('window.ethereum is enabled')
+    if (window.ethereum.isMetaMask === true) {
+      console.log('MetaMask is active')
+      let mmDetected = document.getElementById('mm-detected')
+      mmDetected.innerHTML += 'MetaMask Is Available!'
+      // add in web3 here
+      var web3 = new Web3(window.ethereum)
+    } else {
+      console.log('MetaMask is not available')
+      let mmDetected = document.getElementById('mm-detected')
+      mmDetected.innerHTML += 'MetaMask Not Available!'
+      // let node = document.createTextNode('<p>MetaMask Not Available!<p>')
+      // mmDetected.appendChild(node)
+    }
+  } else {
+    console.log('window.ethereum is not found')
+    let mmDetected = document.getElementById('mm-detected')
+    mmDetected.innerHTML += '<p>MetaMask Not Available!<p>'
+  }
+})
+
+var web3 = new Web3(window.ethereum)
+// Grabbing the button object,
+const mmEnable = document.getElementById('mm-connect');
+// since MetaMask has been detected, we know
+// `ethereum` is an object, so we'll do the canonical
+// MM request to connect the account.
+//
+// typically we only request access to MetaMask when we
+// need the user to do something, but this is just for
+// an example
+mmEnable.onclick = async () => {
+  await ethereum.request({ method: 'eth_requestAccounts'})
+  // grab mm-current-account
+  // and populate it with the current address
+  var mmCurrentAccount = document.getElementById('mm-current-account');
+  mmCurrentAccount.innerHTML = 'Current Account: ' + ethereum.selectedAddress
+
+  var web3 = new Web3(window.ethereum);
+  const rewardProgramContract = new web3.eth.Contract(rewardProgramABI, rewardProgramAddress);
+  rewardProgramContract.setProvider(window.ethereum);
+
+  var displayValue1 = document.getElementById('display-value1');
+  var reward1remaining = await rewardProgramContract.methods.amountRaceTicket().call;
+  displayValue1.innerHTML = "Remaining: " + reward1remaining;
+
+  var displayValue2 = document.getElementById('display-value2');
+  var reward2remaining = await rewardProgramContract.methods.amountVinyardTour().call;
+  displayValue2.innerHTML = "Remaining: " + reward2remaining;
+
+  var displayValue3 = document.getElementById('display-value3');
+  var reward3remaining = await rewardProgramContract.methods.amountRolex().call;
+  displayValue3.innerHTML = "Remaining: " + reward3remaining;
+
+  document.getElementById('reward1-header').style.display = "block";
+  document.getElementById('display-value1').style.display = "block";
+  document.getElementById('redeem-reward1').style.display = "block";
+
+  document.getElementById('reward2-header').style.display = "block";
+  document.getElementById('display-value2').style.display = "block";
+  document.getElementById('redeem-reward2').style.display = "block";
+
+  document.getElementById('reward3-header').style.display = "block";
+  document.getElementById('display-value3').style.display = "block";
+  document.getElementById('redeem-reward3').style.display = "block";
+
+
+
+}
