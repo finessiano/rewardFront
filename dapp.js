4821fcc205bd9349a0e030989bf1a42d17c03b83
finessiano <80067457+finessiano@users.noreply.github.com>
2022-01-20 02:48:53 +0100

    Update dapp.js

--- dapp.js
+++ dapp.js
@@ -224,15 +224,15 @@ mmEnable.onclick = async () => {
   rewardProgramContract.setProvider(window.ethereum);
 
   var displayValue1 = document.getElementById('display-value1');
-  var reward1remaining = await rewardProgramContract.methods.remainingRaceTicket().call;
+  var reward1remaining = await rewardProgramContract.methods.remainingRaceTicket().call();
   displayValue1.innerHTML = "Remaining: " + reward1remaining;
 
   var displayValue2 = document.getElementById('display-value2');
-  var reward2remaining = await rewardProgramContract.methods.remainingVinyardTour().call;
+  var reward2remaining = await rewardProgramContract.methods.remainingVinyardTour().call();
   displayValue2.innerHTML = "Remaining: " + reward2remaining;
 
   var displayValue3 = document.getElementById('display-value3');
-  var reward3remaining = await rewardProgramContract.methods.remainingRolex().call;
+  var reward3remaining = await rewardProgramContract.methods.remainingRolex().call();
   displayValue3.innerHTML = "Remaining: " + reward3remaining;
 
 
